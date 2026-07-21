003b3e2679183f734b3348057db0bd3a8860bdb3
KKondor <kkondor66@gmail.com>
2026-07-21 11:25:12 +0200

    feat: added validation helper to handle type validations

---
+++ JobFollower/Backend/Helpers/ValidationHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobFollower.Backend.Helpers
+{
+    public class ValidationHelper
+    {
+        public static List<ValidationResult> Validate<T>(T obj)
+        {
+            var context = new ValidationContext(obj!);
+            var results = new List<ValidationResult>();
+            Validator.TryValidateObject(obj!, context, results, validateAllProperties: true);
+            return results;
+        }
+        public static Dictionary<string, string[]> ToValidationDictionary(List<ValidationResult> results)
+        {
+            return results.SelectMany(x => x.MemberNames, (result, membernames) => new { membernames, result.ErrorMessage }).GroupBy(x => x.membernames).ToDictionary(g => g.Key, g => g.Select(x => x.ErrorMessage).ToArray());
+        }
+    }
+}
