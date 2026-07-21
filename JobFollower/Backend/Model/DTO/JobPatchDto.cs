065c8b6dcf2debb46802eeb508c0033fd6d592f2
KKondor <kkondor66@gmail.com>
2026-07-21 11:26:57 +0200

    feat: created JobPatchDto to handle patch update requests

---
+++ JobFollower/Backend/Model/DTO/JobPatchDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static JobFollower.Backend.Model.JobApplication;
+
+namespace JobFollower.Backend.Model.DTO
+{
+    public class JobPatchDto
+    {
+        [MaxLength(100)]
+        public String? JobName { get; set; }
+        [MaxLength(1000)]
+        public String? JobDescription { get; set; }
+        [Range(0,5)]
+        public StatusState? Status { get; set; } = StatusState.NotApplied;
+        public DateTime? AppliedDate { get; set; }
+    }
+}
